82463ed05348fdadc6d00dcdab2df74ae77aed73
joel <joelfoy@gmail.com>
2011-07-22 16:02:25 -0400

    initial_07222011

---
+++ index.php
@@ -0,0 +1,226 @@
+<!doctype html>
+
+<!--[if IEMobile 7 ]> <html lang="en" class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width">
+  
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!--  Mobile Viewport Fix
+        j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
+  device-width : Occupy full width of the screen in its current orientation
+  initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
+  maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
+  -->
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+
+  <!-- CSS : implied media="all" -->
+  <link rel="stylesheet" href="css/style.css" />
+  <!--  <link rel="stylesheet" href="css/style.css?v=1">  -->
+
+  <!-- For the less-enabled mobile browsers like Opera Mini -->
+  <link rel="stylesheet" media="handheld" href="css/handheld.css?v=1">
+
+ 
+  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+  <script src="js/modernizr-1.5.min.js"></script>
+
+</head>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+
+<!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <body class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <body> <!--<![endif]-->
+
+<!-- Body -->
+
+	<!-- Floating Navigation -->
+	<div id="floating-nav">
+		<nav role="navigation">
+			<ul>
+				<li id="intro-nav-float" class="float-nav-link"><a href="#home">home</a><span>home</span></li>
+				<li id="about-nav-float" class="float-nav-link"><a href="#about">about</a><span>about</span></li>
+				<li id="portfolio-nav-float" class="float-nav-link"><a href="#portfolio">portfolio</a><span>portfolio</span></li>
+				<li id="contact-nav-float" class="float-nav-link"><a href="#contact">contact</a><span>contact</span></li>
+			</ul>
+		</nav>
+	</div>
+
+
+  <div id="container">
+  	
+  	<!-- Home Section -->
+	<div id="home">
+		<header role="banner">
+			<div id="logo">creative|switch</div>
+			<nav role="navigation" id="main-navigation">
+				<ul>
+					<li id="about-nav" class="main-nav-link"><a href="#about">about</a></li>
+					<li id="portfolio-nav" class="main-nav-link"><a href="#portfolio">portfolio</a></li>
+					<li id="contact-nav" class="main-nav-link"><a href="#contact">contact</a></li>
+				</ul>
+			</nav>
+		</header>
+		<img id="me-standing" class="me-photo" src="images/me-standing.png" />
+		<section id="intro">Hello, I'm <span class="me">Joel Foy</span>. I am <em>passionate</em> about minimal, clean and simple <em>web designs</em> that are easy to navigate and have <em>user friendly</em> designs. The goal is to create a site for you that <em>engages</em> your <em>audience</em> or customers.</section>
+	</div><!-- end Home -->
+	
+	<!-- Get Started -->
+	<div id="get-started">
+		<p>Are you ready to start your website?</p>
+		<a href="#contact">Contact</a>
+	</div>
+	<!-- end Get Started -->
+	
+	<!-- About Section -->
+	<div id="about">
+		<section id="about-me">
+			<section id="who-i-am">
+				<h2>About Me</h2>
+				<p>I am an Atlanta based graphic designer and web designer/developer. I do everything from hand coding to photography to video production. I love the process of pulling projects together from concept to completed project.</p> 
+				<p>My projects utilize Photoshop, Illustrator, HTML & CSS, PHP, jQuery and many more.</p>
+			</section>
+		</section>
+	</div>
+	<!-- end About Section -->
+	
+	<!-- Portfolio Section -->
+	<div id="portfolio">
+		<section id="my-work">
+			<h2>Here is a preview of <span class="me">my work</span>.</h2>
+			<h3>web, print &amp; branding</h3>
+		</section>
+		
+		<section id="work-examples" class="clearfix">
+			<!-- Christopher S. Bryan -->
+			<div class="mosaic-block bar">
+				<a href="http://www.christophersbryancpa.com" target="_blank" class="mosaic-overlay">
+					<div class="details">
+						<h4>Christopher S. Bryan, CPA</h4>
+						<p>Branding, Print &amp; Web Design</p>
+					</div>
+				</a>
+				<div class="mosaic-backdrop"><img src="images/clients/csbcpa.jpg"/></div>
+			</div>
+			<!-- c-level communications -->
+			<div class="mosaic-block bar">
+				<a href="#" target="_blank" class="mosaic-overlay">
+					<div class="details">
+						<h4>C-Level Communications</h4>
+						<p>Branding &amp; Print</p>
+					</div>
+				</a>
+				<div class="mosaic-backdrop"><img src="images/clients/c-level.jpg"/></div>
+			</div>
+			<!-- Harris Equipment -->
+			<div class="mosaic-block bar">
+				<a href="http://www.harrisequip.com" target="_blank" class="mosaic-overlay">
+					<div class="details">
+						<h4>Harris</h4>
+						<p>Branding, Print &amp; Web Design</p>
+					</div>
+				</a>
+				<div class="mosaic-backdrop"><img src="images/clients/harris.jpg"/></div>
+			</div>
+			<!-- Avexio -->
+			<div class="mosaic-block bar">
+				<a href="http://www.harrisequip.com" target="_blank" class="mosaic-overlay">
+					<div class="details">
+						<h4>Avexio</h4>
+						<p>Branding</p>
+					</div>
+				</a>
+				<div class="mosaic-backdrop"><img src="images/clients/avexio.jpg"/></div>
+			</div>
+			<!-- Wilkerson Packaging -->
+			<div class="mosaic-block bar">
+				<a href="http://www.wilkersonpkg.com/" target="_blank" class="mosaic-overlay">
+					<div class="details">
+						<h4>Wilkerson Packaging</h4>
+						<p>Web Design</p>
+					</div>
+				</a>
+				<div class="mosaic-backdrop"><img src="images/clients/wilkerson.jpg"/></div>
+			</div>
+			<!-- Label Tech -->
+			<div class="mosaic-block bar">
+				<a href="http://www.label-tech.net/" target="_blank" class="mosaic-overlay">
+					<div class="details">
+						<h4>Labeltech Converting</h4>
+						<p>Web Design</p>
+					</div>
+				</a>
+				<div class="mosaic-backdrop"><img src="images/clients/label-tech.jpg"/></div>
+			</div>
+		
+		</section><!-- end work examples -->
+		
+	</div>
+	<!-- end Portfolio Section -->
+    
+    <!-- Contact Section -->
+    <div id="contact">
+    	<section id="contact-me">
+    		<p>This is contact info.</p>
+    	</section>
+    	<section id="contact-form">
+    	
+    	</section>
+    </div>
+    <!-- end Contact Section -->
+    
+  </div> <!--! end of #container -->
+
+
+  <!-- Javascript at the bottom for fast page loading -->
+
+  <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  <script>!window.jQuery && document.write('<script src="js/jquery-1.4.2.min.js"><\/script>')</script>
+  
+  <!-- jQuery Tools plugin -->
+  <script src="js/jquery.tools.min.js"></script>
+  <script src="js/mosaic.1.0.1.js?v=1"></script>
+  <script src="js/plugins.js?v=1"></script>
+  <script src="js/script.js?v=1"></script>
+
+  <!--[if lt IE 7 ]>
+    <script src="js/dd_belatedpng.js?v=1"></script>
+  <![endif]-->
+
+  <!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet 
+       change the UA-XXXXX-X to be your site's ID -->
+  <script>
+   var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+   (function(d, t) {
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.async = true;
+    g.src = '//www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s);
+   })(document, 'script');
+  </script>
+  
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,60 @@
+/* Author: Joel Foy
+
+*/
+$(document).ready(function() {
+
+	// hide floating nav
+	$('#floating-nav').hide();
+
+	// fade in floating menu
+	$(function() {
+		$(window).scroll(function(){
+			if($(this).scrollTop() > 100) {
+				$('#floating-nav').fadeIn();
+			} else {
+				$('#floating-nav').fadeOut();
+			}
+		});
+	
+	}); //end floating menu fade
+	
+	// Nav Scrolling 
+	
+	$('nav ul a').bind('click',function(event){
+		var $anchor = $(this);
+		
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1000);
+		event.preventDefault();
+	});	
+
+	// Mosiac for portfolio
+	
+	$('.bar').mosaic({
+		animation	:	'slide'		//fade or slide
+	});
+
+}); // end of jQuery
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
